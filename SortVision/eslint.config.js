d4ccc4cd580d334f67abf08159cdd5bd0f015753
alienx5499 <p2005p5499p@gmail.com>
2025-07-09 14:55:34 +0530

    Enhance ESLint configuration: Add .next to ignored files, include Node globals, and update 'no-unused-vars' rule to warn for unused arguments and destructured arrays. Also, add warning for 'react-hooks/exhaustive-deps' rule.

--- SortVision/eslint.config.js
+++ SortVision/eslint.config.js
@@ -4,12 +4,16 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', '.next'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        process: 'readonly',
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -23,11 +27,16 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^([A-Z_]|_)' }],
+      'no-unused-vars': ['warn', { 
+        varsIgnorePattern: '^([A-Z_]|_)',
+        argsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_'
+      }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
 ]
