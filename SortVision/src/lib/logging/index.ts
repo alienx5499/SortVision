f9f265edba5a2f18c94295af8d489c2329f1111c
alienx5499 <p2005p5499p@gmail.com>
2026-05-10 19:40:23 +0530

    refactor(components): remove unused exports and consolidate component

--- SortVision/src/lib/logging/index.ts
+++
@@ -1,7 +0,0 @@
-// knip-disable-next-line -- barrel export for public API, imported by API routes
-export {
-  getOrCreateCorrelationId,
-  correlationHeaders,
-} from './correlationId.ts';
-// knip-disable-next-line -- barrel export for public API
-export { createServerLogger } from './createServerLogger.ts';
